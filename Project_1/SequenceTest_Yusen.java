2aff6d638f7850e5ae04cf8df8d8cb7d49bc35d7
Afeique Sheikh <afeique@gmail.com>
2016-10-15 19:46:21 -0400

    merged dropbox files in

---
+++ Project_1/SequenceTest_Yusen.java
@@ -0,0 +1,165 @@
+// Created by: Yusen Jiang, Sydeny McDaniel, Afeique Sheikh
+
+//This class will present the user with a menu for different options that will test all of the methods in the DoubleArraySeq class.
+//There are two arrays that are already created and then presented to the user for them to use in the testing process.
+//Some of the options, such as 5, 6, 7, 8, 10, and 12 will ask for user input and then use that to complete the task.
+//Other methods are simply activated and then the action is completed, depending on which option is chosen, the results
+//will be printed out for the user to see. This is to ensure that the correct action was performed.
+//Throughout the code there are exceptions thrown to further ensure that the methods are tested properly and the input given is 
+//the correct input for that scenario.
+
+import java.util.Scanner; 
+
+public class SequenceTest {
+	public static int activeSeq = 0;
+	public static String activeAlpha = "A";
+
+	public static void UpdateActiveSeq() {
+		System.out.println("");
+		SequenceTest.activeAlpha = "Unknown";
+		if (SequenceTest.activeSeq == 0)
+			SequenceTest.activeAlpha = "A";
+		else if (SequenceTest.activeSeq == 1)
+			SequenceTest.activeAlpha = "B";
+	}
+
+	public static void PrintMenu() {
+		System.out.println("\n\nMenu Selection");
+		System.out.println("-----------------------------------------------------------------------------");
+		System.out.println("1. Print the sequences of A and B");
+		System.out.println("2. Report the capacities of A and B");
+		System.out.println("3. Report if A and B are equal");
+		System.out.println("4. Toggle the ACTIVE sequence ("+ SequenceTest.activeAlpha +" is curently ACTIVE)" );
+		System.out.println("5. Add a number to the front of the ACTIVE sequence");
+		System.out.println("6. Add a number before a specified number in the ACTIVE sequence");
+		System.out.println("7. Add a number after a specified number in the ACTIVE sequence");
+		System.out.println("8. Add a number to the end of the ACTIVE sequence");
+		System.out.println("9. Add sequence B to end of A");
+		System.out.println("10. Delete a number at a certain index from the ACTIVE sequence");
+		System.out.println("11. Delete the first number from the ACTIVE sequence");
+		System.out.println("12. Display a number at a certain index in the ACTIVE sequence");
+		System.out.println("13. Display the last element in the ACTIVE sequence");
+		System.out.println("14. Trim extra memory from both A and B");
+		System.out.println("15. Create a clone of the ACTIVE sequence and show");
+		System.out.println("16. Create a new sequence by concatenating B to A");
+		System.out.println("17. Quit");
+		System.out.println("-----------------------------------------------------------------------------\n");
+	}
+
+	public static void main(String [] arg)
+	{
+		DoubleArraySeq[] s = {new DoubleArraySeq(), new DoubleArraySeq()};
+		SequenceTest.activeSeq = 0;
+		Scanner scanner = new Scanner(System.in);
+		
+		int input;
+		double element;
+		int index;
+		
+		dance:
+		while (true) {
+			SequenceTest.PrintMenu();
+			SequenceTest.UpdateActiveSeq();
+			System.out.println("ACTIVE SEQUENCE: "+ SequenceTest.activeAlpha);
+			
+			//choose menu 
+			System.out.println("Enter the menu selection: ");
+			input = scanner.nextInt();
+
+			String out = "";
+			switch (input) {
+				case 1:
+					System.out.println("\tA: " + s[0].toString() +"\n\tB: " + s[1].toString());
+					break;
+				case 2:
+					System.out.println("\tA: "+ s[0].getCapacity() +"\n\tB: "+ s[1].getCapacity());
+					break;
+				case 3:
+					if (s[0].equals(s[1]))
+						System.out.println("\tA and b are equal");
+					else
+						System.out.println("\tA and B are not equal");
+
+					if (s[0].getCapacity() == 0 && s[1].getCapacity() == 0)
+						System.out.println("\n\t(A and B are both empty)");
+					break;
+				case 4:
+					if (SequenceTest.activeSeq == 0)
+						SequenceTest.activeSeq = 1;
+					else if (SequenceTest.activeSeq == 1)
+						SequenceTest.activeSeq = 0;
+					SequenceTest.UpdateActiveSeq();
+					System.out.println("\tActive sequence switched to: "+ SequenceTest.activeAlpha);
+					break;
+				case 5:
+					System.out.println("\nEnter the number to add to the front of sequence "+ SequenceTest.activeAlpha +": ");
+					element = scanner.nextDouble();
+					s[SequenceTest.activeSeq].addFront(element);
+					System.out.printf("Added %.2f to the front of sequence %s", element, SequenceTest.activeAlpha);
+					break;
+				case 6:
+					break;
+				case 7:
+					break;
+				case 8:
+					System.out.println("\nEnter the number to add to the end of sequence "+ SequenceTest.activeAlpha +": ");
+					element = scanner.nextDouble();
+					s[SequenceTest.activeSeq].addEnd(element);
+					System.out.printf("Added %.2f to the end of sequence %s", element, SequenceTest.activeAlpha);
+					break;
+				case 9:
+					out = "\tA: " + s[0].toString() +"\n\tB: " + s[1].toString();
+					s[0].addAll(s[1]);
+					out += "\n\tA + B = "+ s[0].toString();
+					out += "\n\n\tAdded sequence B to end of A";
+					System.out.println(out);
+					break;
+			/*	case 10:
+					System.out.println("\nEnter the index number of the element to delete (0, 1, 2,...): ");
+					index = scanner.nextInt();
+					s[SequenceTest.activeSeq].setCurrent(index);
+					System.out.println("Element at index %d: %.2f", index, s[SequenceTest.activeSeq].getElement());
+					*/
+
+				case 11:		
+					
+					s[SequenceTest.activeSeq].removeFront();
+					break;
+					
+				case 12:
+					System.out.println("\nEnter the index number your looking for "+ SequenceTest.activeAlpha +": ");
+					index = scanner.nextInt();
+					
+					System.out.println(index);
+					
+					try {
+						s[SequenceTest.activeSeq].getElement(index);
+				      } catch (IllegalStateException e) {
+				         System.out.println(e.getMessage());
+				      }
+					
+					break;
+					
+					
+				case 13:
+					break;
+				case 14:
+					s[0].trimToSize();
+					s[1].trimToSize();			
+					break;
+					
+				case 15:
+					DoubleArraySeq copy = s[SequenceTest.activeSeq].clone();
+					System.out.printf("ACTIVE SEQUENCE CLONE: %s", copy.toString());
+					break;
+				case 16:
+					break;
+				case 17:
+					break dance;
+				default:
+					throw new IllegalArgumentException("Whatchu tryin' to pull. That ain't no choice. Make my day and choose again, punk.");
+			}
+
+		}
+	}
+}
