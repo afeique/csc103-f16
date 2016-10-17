823e2e3990d4d7603a603bd9fa7b5170fb13b3ef
Afeique Sheikh <afeique@gmail.com>
2016-10-17 00:48:39 -0400

    implemented add using IntNodes instead of arrays

--- Project_2/UnboundedInt.java
+++ Project_2/UnboundedInt.java
@@ -1,36 +1,50 @@
 
 import java.math.BigInteger;
 
-public class UnboundedInt implements Cloneable {
-    int sign, numNodes;
-    IntNode head, tail, cursor;
+public class UnboundedInt {
+    private int sign, numNodes;
+    private IntNode head, tail, cursor;
 
+    private UnboundedInt() {
+        // assume sign is positive
+        this.sign = 1;
+        // start with no linked list
+        this.head = null;
+        this.tail = null;
+        this.numNodes = 0;
+        // we ain't got nothin' to point to
+        this.cursor = null;
+    }
+
+    /**
+     * Overloaded constructor that takes a java.math.BigInteger and uses that
+     * to set the value of the UnboundedInt.
+     *
+     * @param BigInteger
+     *   The BigInteger with the value that this UnboundedInt should be initialized to.
+     */
     public UnboundedInt(BigInteger b) {
         this(b.toString());
     }
 
     public UnboundedInt(String input) {
+        // initialize values
+        this();
+
         // sanitize input String to avoid NumberFormatException
-
-        // assume sign is positive
-        this.sign = 1;
-
-        // start with no linked list
-        this.head = null;
 
         // check if negative sign at start of input, set sign accordingly
         if (input.charAt(0) == '-')
             this.sign = -1;
 
         // use regex to remove all non-digits from input
-        // (no scientific notation, kthxbai)
         input = input.replaceAll("[^\\d]+", "");
 
         int len = input.length();
 
         // if the string is empty, then no digits were in the input
         if (len == 0)
-            throw new NumberFormatException("String must contain one or more digits");
+            throw new NumberFormatException("String must contain numeric digits");
 
         // each node contains a 3-digit number 0-999:
         // if we take the input string's length, divide by 3,
@@ -44,23 +58,25 @@ public class UnboundedInt implements Cloneable {
 
         // now we need to loop through the input string BACKWARDS
         // this is because the ones place starts at the end of the string (rightmost char)
-        // reverse the input string for traversal
-        //System.out.format("Original input: %s\n", input);
-        input = new StringBuilder(input).reverse().toString();
-        //System.out.format("Reversed input: %s\n", input);
-
-        // traverse the reversed string and create nodes with each group of 3 digits/chars
-        // create the linked list in reverse order, starting from the tail
-        for (int i=0; i<len; i+=3) {
+        // use i to loop backwards through the input string, hopping 3 digits each iteration
+        // create a new node each iteration of i
+        for (int i=len-1; i>-1; i-=3) {
             // create a char[] to hold this node's value in string format, default=000
             char[] digit = {'0', '0', '0'};
 
-            for (int j=0; j<3; j++) {
-                int k = i+j;
-                if (k >= len)
-                    break;
+            // loop through i-2, i-1, i-0 (in that order)
+            // loop j through 2, 1, 0 (in that order)
+            // so k = i-j will give us the index in the input string for the current digit
+            for (int j=2; j>-1; j--) {
+                // current digit's index in the original input string
+                int k = i-j;
+                // if k is negative, we're past the end (leftmost char) of the input string
+                if (k < 0)
+                    continue;
+
+                // otherwise, if k is positive, overwrite the digit in the char[]
+                // the digit we should overwrite is at index 2-j
                 digit[2-j] = input.charAt(k);
-                //System.out.format("2-j = %d: %s %s %s\n", 2-j, digit[0], digit[1], digit[2]);
             }
 
             // create a string from the char[]
@@ -69,22 +85,20 @@ public class UnboundedInt implements Cloneable {
             nodeInt = Integer.parseInt(nodeStr);
             // finally, create a node using the parsed integer value
             newNode = new IntNode(nodeInt, null);
-
             if (prevNode == null) {
                 // if this is the first node we're creating (prevNode null)
-                // then maintain a reference to this newNode as the tail
-                this.tail = newNode;
+                // then maintain a reference to this newNode as the head
+                this.head = newNode;
             } else {
-                // otherwise set this newNode to point to the prevNode
-                newNode.setLink(prevNode);
+                // otherwise set the prevNode to point to this newNode
+                prevNode.setLink(newNode);
             }
 
             // set prevNode to the newNode, then continue to next iteration
             prevNode = newNode;
         }
 
-        // set the head
-        this.head = prevNode;
+        this.tail = prevNode;
     }
 
     /**
@@ -97,18 +111,107 @@ public class UnboundedInt implements Cloneable {
      */
     public String toString() {
         IntNode curNode = this.head;
-        String out = String.format("%d", curNode.getData());
-        curNode = this.head.getLink();
+        String out, nodeStr;
+        out = "";
+
+        // the nodes are stored from least significant to most significant digits
+        // thus, we have to prepend each successive node to the running String out
+        // the tail node represents the most significant digits: it should NOT be rendered with left-padded zeros
         while (curNode != null) {
-            // use String.format() to left-pad with zeros so length is always 3 digits
-            out = out + String.format("%03d", curNode.getData());
+            if (curNode == this.tail) {
+                // if the node is the tail (most significant digits),
+                // render it without any left-padded zeros
+                //System.out.format("\ntoStr: on tail node: %d\n", curNode.getData());
+                nodeStr = String.format("%d", curNode.getData());
+            } else {
+                // if the node is not the tail (most significant digits),
+                // use String.format() to left-pad with zeros so length is always 3 digits
+                //System.out.format("\ntoStr: on normal node: %03d\n", curNode.getData());
+                nodeStr = String.format("%03d", curNode.getData());
+            }
+
+            out = nodeStr + out;
+            //System.out.format("\nout: %s\n", out);
             curNode = curNode.getLink();
         }
 
-        if (sign == -1)
+        // prepend with a negative sign if necessary
+        if (this.sign < 0)
             out = "-" + out;
 
         return out;
+    }
+
+    /**
+     * A re-implementation of the Integer.parseInt() static method.
+     *
+     * @param input
+     *   The string to convert to an integer
+     * @precondition
+     *   String input contains one or more numeric digits
+     */ 
+    public static int parseInt(String input) {
+        // sanitize input String
+
+        // assume sign is positive
+        int sign = 1;
+
+        // check if negative sign at start of input, set sign accordingly
+        if (input.charAt(0) == '-')
+            sign = -1;
+
+        // use regex to remove all non-digits from input
+        // (no scientific notation, kthxbai)
+        input = input.replaceAll("[^\\d]+", "");
+
+        int len = input.length();
+
+        // if the string is empty, then no digits were in the input
+        if (len == 0)
+            throw new NumberFormatException("String must contain one or more digits");
+
+        char digit;
+        int asciiCode;
+        int pwr = 0;
+        int num = 0;
+
+        // loop through input string right-to-left (least significant digit to most significant)
+        for (int i=len-1; i>-1; i--) {
+            // get char digit at current position
+            digit = input.charAt(i);
+            // cast digit char as int to get ascii code
+            asciiCode = (int)digit;
+
+            // convert asciiCode to actual int digit, multiply digit by 10^(pwr), add to num
+            num += UnboundedInt.asciiDigitLookup(asciiCode) * Math.pow(10, pwr++);
+        }
+
+        return sign*num;
+    }
+
+    /**
+     * Lookup and return the numeric digit an asciiCode represents as an int.
+     * Throw an IllegalArgumentException if the asciiCode input does not represent a numeric digit.
+     *
+     * @param asciiCode
+     *   Primitive int representing the ASCII code to look up.
+     * @precondition
+     *   Input asciiCode is the code for a numeric digit [0-9], i.e. within range [48-57]
+     * @return
+     *   The digit the asciiCode input represents as an integer.
+     * @exception IllegalArgumentException
+     *   Thrown if the asciiCode input does not represent a numeric digit (code outside of range [48-57])
+     */
+    public static int asciiDigitLookup(int asciiCode) {
+        // ascii code 48 = digit 0; ascii code 57 = digit 9
+        // by subtracting the asciiCode input from 48, we get the exact int digit it represents
+        int digit = asciiCode - 48;
+
+        // make sure the digit is within bounds
+        if (digit < 0 || digit > 9)
+            throw new IllegalArgumentException("ASCII code "+ asciiCode +" does not correspond to a digit [0-9]");
+
+        return digit;
     }
 
     public void start() {
@@ -145,16 +248,6 @@ public class UnboundedInt implements Cloneable {
         return new BigInteger(this.toString());
     }
 
-    public UnboundedInt add(UnboundedInt a) {
-        BigInteger result = this.toBigInteger().add(a.toBigInteger());
-        return new UnboundedInt(result);
-    }
-
-    public UnboundedInt multiply(UnboundedInt m) {
-        BigInteger result = this.toBigInteger().multiply(m.toBigInteger());
-        return new UnboundedInt(result);
-    }
-
     public boolean equals(Object o) {
         if (o instanceof UnboundedInt) {
             UnboundedInt u = (UnboundedInt)o;
@@ -170,5 +263,246 @@ public class UnboundedInt implements Cloneable {
         return false;
     }
 
+
+    /**
+     * Add another UnboundedInt to the current one, returning a new UnboundedInt object as the result.
+     *
+     * @param UnboundedInt
+     *   The other UnboundedInt to add.
+     * @return UnboundedInt
+     *   A new UnboundedInt containing the sum of the two UnboundedInts.
+     */
+    public UnboundedInt add(UnboundedInt u) {
+        int[] val = {0, 0};
+        // addition between an N digit/node number and an M digit/node number
+        // results in a sum of at most max(N,M)+1 digits/nodes
+        // ex: 99+999 = 1098 (2 digits, 3 digits => max(2,3)+1 = 4 digits)
+        int sumNumNodes = Math.max(this.numNodes, u.numNodes)+1;
+        UnboundedInt sum = new UnboundedInt();
+        int overflow, sumNode, overflowNode;
+        String sumStr;
+
+        // If the addend has a different number of nodes than this, at some point one of the linked lists
+        // will reach its end while the other will have more nodes to traverse.
+
+        // Thus, one of the linked lists will have its cursor=null, and will start throwing exceptions
+        // as we try to continue advancing and trying to retrieve the cursor node's value.
+        
+        // However, because these Exceptions are expected, we'll isolate them using try/catch blocks
+        // and ignore them, continuing operation as expected.
+
+        //System.out.format("this.numNodes: %d\nu.numNodes: %d\nsumNumNodes: %d\n", this.numNodes, u.numNodes, sumNumNodes);
+
+        // create the first two nodes of the sum
+        sum.head = new IntNode(0, new IntNode(0, null));
+        sum.tail = sum.head.getLink();
+        sum.numNodes = 2;
+        // position sum's cursor at the head
+        sum.start();
+
+        // start each linked list at its respective head
+        this.start(); u.start();
+        for (int i=0; i<sumNumNodes-1; i++) {
+            // get the node values, handling expected Exceptions
+            try {
+                val[0] = this.cursor.getData();
+            } catch (IllegalStateException e) {
+                val[0] = 0;
+            }
+            try {
+                val[1] = u.cursor.getData();
+            } catch (IllegalStateException e) {
+                val[1] = 0;
+            }
+
+            sumNode = sum.cursor.getData() + (this.sign*val[0]) + (u.sign*val[1]);
+            overflowNode = sum.cursor.getLink().getData();
+
+            // use integer division to determine if there is overflow
+            overflow = sumNode/1000;
+
+            //System.out.format("sum[%d]: %d\noverflow: %d\n", i, sum[i], overflow);
+
+            // handle the overflow and carry over
+            if (overflow > 0) {
+                sumNode -= 1000;
+                overflowNode += 1;
+            }
+
+            // commit our local variables to the sum IntNodes
+            sum.cursor.setData(sumNode);
+            sum.cursor.getLink().setData(overflowNode);
+
+            // advance the sum's cursor to the next node (which is the current overflow node)
+            sum.advance();
+            sum.tail = new IntNode(0, null);
+            sum.cursor.setLink(sum.tail);
+            sum.numNodes += 1;
+
+            //System.out.format("sum[%d]: %d\nsum[%d+1]: %d\n", i, sum[i], i, sum[i+1]);
+
+            // advance the linked list cursors, handling expected Exceptions
+            try {
+                this.advance();
+            } catch (IllegalStateException e) {
+                // do nothing
+            }
+            try {
+                u.advance();
+            } catch (IllegalStateException e) {
+                // do nothing
+            }
+        }
+
+        // trim off unused (zero) nodes at the end
+        sum.trim();
+
+        // deal with negative nodes, carry-right, and negative signage
+        sum.start();
+        while (sum.cursor != sum.tail) {
+            sumNode = sum.cursor.getData();
+            overflowNode = sum.cursor.getLink().getData();
+            if (sumNode < 0) {
+                overflowNode -= 1;
+                sumNode += 1000;
+            }
+            sum.cursor.setData(Math.abs(sumNode));
+            sum.cursor.getLink().setData(overflowNode);
+            sum.advance();
+        }
+        sumNode = sum.tail.getData();
+        if (sumNode < 0) {
+            sum.tail.setData(Math.abs(sumNode));
+            sum.sign = -1;
+        }
+
+        sum.trim();
+
+        /*
+        sumStr = UnboundedInt.resultArrayToString(sum);
+        return new UnboundedInt(sumStr);
+        */
+        return sum;
+    }
+
+    public void trim() {
+        while (this.tail.getData() == 0) {
+            if (this.numNodes == 1)
+                break;
+            this.start();
+            while (this.cursor.getLink() != this.tail) {
+                this.advance();
+            }
+            this.cursor.setLink(null);
+            this.tail = this.cursor;
+            this.numNodes -= 1;
+        }
+    }
+
+
+    /*
+    public UnboundedInt add2(UnboundedInt u) {
+        int[] val = {0, 0};
+        // addition between an N digit/node number and an M digit/node number
+        // results in a sum of at most max(N,M)+1 digits/nodes
+        // ex: 99+999 = 1098 (2 digits, 3 digits => max(2,3)+1 = 4 digits)
+        int sumNumNodes = Math.max(this.numNodes, u.numNodes)+1;
+        int[] sum = new int[sumNumNodes];
+        int overflow;
+        String sumStr;
+
+        // If the addend has a different number of nodes than this, at some point one of the linked lists
+        // will reach its end while the other will have more nodes to traverse.
+
+        // Thus, one of the linked lists will have its cursor=null, and will start throwing exceptions
+        // as we try to continue advancing and trying to retrieve the cursor node's value.
+        
+        // However, because these Exceptions are expected, we'll isolate them using try/catch blocks
+        // and ignore them, continuing operation as expected.
+
+        //System.out.format("this.numNodes: %d\nu.numNodes: %d\nsumNumNodes: %d\n", this.numNodes, u.numNodes, sumNumNodes);
+
+        // start each linked list at its respective head
+        this.start(); u.start();
+        for (int i=0; i<sumNumNodes-1; i++) {
+            // get the node values, handling expected Exceptions
+            try {
+                val[0] = this.getNodeValue();
+            } catch (IllegalStateException e) {
+                val[0] = 0;
+            }
+            try {
+                val[1] = u.getNodeValue();
+            } catch (IllegalStateException e) {
+                val[1] = 0;
+            }
+
+            sum[i] += (this.sign*val[0]) + (u.sign*val[1]);
+            // use integer division to determine if there is overflow
+            overflow = sum[i]/1000;
+            //System.out.format("sum[%d]: %d\noverflow: %d\n", i, sum[i], overflow);
+
+            // handle the overflow and carry over
+            if (overflow > 0) {
+                sum[i] -= 1000;
+                sum[i+1] += 1;
+            }
+            //System.out.format("sum[%d]: %d\nsum[%d+1]: %d\n", i, sum[i], i, sum[i+1]);
+
+            // advance the cursors, handling expected Exceptions
+            try {
+                this.advance();
+            } catch (IllegalStateException e) {
+                // do nothing
+            }
+            try {
+                u.advance();
+            } catch (IllegalStateException e) {
+                // do nothing
+            }
+        }
+
+        // loop over the result array and create a 
+        sumStr = UnboundedInt.resultArrayToString(sum);
+        return new UnboundedInt(sumStr);
+    }
+    */
+
+    /**
+     * Loop over a particular result array (an int[] with each entry a particular value for a node)
+     * and create a String from it.
+     *
+     * @param result
+     *   int[] containing the results to convert to a String.
+     * @return String
+     *   String output of the results.
+     */
+    /*
+    private static String resultArrayToString(int[] result) {
+        String out = "";
+        for (int i=0; i<result.length; i++) {
+            // if we're on the last entry of the array and it == 0,
+            // break, otherwise we'll end up left-padding the output with an unnecessary 0
+            if (i == result.length-1 && result[i] == 0)
+                break;
+            out = Integer.toString(result[i]) + out;
+        }
+        if (result[0] < 0)
+            out = "-" + out;
+        return out;
+    }
+    */
+
+    public UnboundedInt multiply(UnboundedInt u) {
+        // multiplication of an N digit/node number by an M digit/node number
+        // results in a product of at most N+M digits
+        // example: 99*99 = 9801 (2+2 = 4 digits)
+        int prodNumNodes = this.numNodes + u.numNodes;
+        int[] prod = new int[prodNumNodes];
+
+
+        System.out.format("this.numNodes: %d\nu.numNodes: %d\nprodNumNodes: %d\n", this.numNodes, u.numNodes, prodNumNodes);
+        return new UnboundedInt("0");
+    }
 
 }
--- Project_2/UnboundedIntTests.java
+++ Project_2/UnboundedIntTests.java
@@ -3,12 +3,15 @@ import java.math.BigInteger;
 
 public class UnboundedIntTests {
 	public static void main(String[] args) {
-		UnboundedInt u1, u2, usum, uprod;
+		UnboundedInt u1, u2, usum, uprod, clone;
 		BigInteger b1, b2, bsum, bprod;
 		String[] val;
 
 		String[][] testValues = {
+			{"9999", "9999"},
+			{"-9999", "-9999"},
 			{"+123,456,789", "+987,654,321"},
+			{"-123,456,789", "-987,654,321"},
 			{"-1", "-1"},
 			{"-1", "+1"},
 			{"123", "456"},
@@ -30,9 +33,9 @@ public class UnboundedIntTests {
 			u2 = new UnboundedInt(val[1]);
 			System.out.format("Created UnboundedInt u2 = %s\n", u2.toString());
 			usum = u1.add(u2);
-			System.out.format("u1 + u2 = %s\n", usum.toString());
+			System.out.format("u1+u2 = %s\n", usum.toString());
 			uprod = u1.multiply(u2);
-			System.out.format("u1 * u2 = %s\n", uprod.toString());
+			System.out.format("u1*u2 = %s\n", uprod.toString());
 
 			System.out.println("");
 
@@ -41,37 +44,54 @@ public class UnboundedIntTests {
 			b2 = new BigInteger(u2.toString());
 			System.out.format("Created BigInteger b2 = %s\n", b2.toString());
 			bsum = b1.add(b2);
-			System.out.format("b1 + b2 = %s\n", bsum.toString());
+			System.out.format("b1+b2 = %s\n", bsum.toString());
 			bprod = b1.multiply(b2);
-			System.out.format("b1 * b2 = %s\n", bprod.toString());
+			System.out.format("b1*b2 = %s\n", bprod.toString());
 
 			System.out.println("");
 
 			r = u1.equals(b1);
 			assert r;
-			System.out.println("u1 == b1 : "+ r);
-
+			System.out.format("u1==b1 :: %s == %s : %s\n", u1, b1, r);
 			r = u2.equals(b2);
 			assert r;
-			System.out.println("u2 == b2 : "+ r);
+			System.out.format("u2==b2 :: %s == %s : %s\n", u2, b2, r);
+			System.out.println("");
 
-			r = !u1.equals(b2);
-			assert r;
-			System.out.println("u1 != b2 : "+ r);
-
-			r = !u2.equals(b1);
-			assert r;
-			System.out.println("u2 != b1 : "+ r);
 
 			r = usum.equals(bsum);
 			assert r;
-			System.out.println("u1+u2 == b1+b2 : "+ r);
-
+			System.out.format("u1+u2 == b1+b2 :: %s == %s : %s\n", usum, bsum, r);
 			r = uprod.equals(bprod);
 			assert r;
-			System.out.println("u1*u2 == b1*b2 : "+ r);
-
+			System.out.format("u1*u2 == b1*b2 :: %s == %s : %s\n", uprod, bprod, r);
 			System.out.println("");
+
+			clone = u1.clone();
+			r = u1.equals(clone);
+			assert r;
+			System.out.format("u1.clone() == u1 :: %s == %s : %s\n", clone, u1, r);
+			clone = u2.clone();
+			r = u2.equals(clone);
+			assert r;
+			System.out.format("u2.clone() == u2 :: %s == %s : %s\n", clone, u2, r);
+			System.out.println("");
+
+			/*
+			r = u1.equals(u2);
+			System.out.format("u1==u2 :: %s == %s : %s\n", u1, u2, r);
+			r = b1.equals(b2);
+			System.out.format("b1==b2 :: %s == %s : %s\n", b1, b2, r);
+			r = u1.equals(b2);
+			System.out.format("u1==b2 :: %s == %s : %s\n", u1, b2, r);
+			r = u2.equals(b1);
+			System.out.format("u2==b1 :: %s == %s : %s\n", u2, b1, r);
+			r = b1.equals(u2);
+			System.out.format("b1==u2 :: %s == %s : %s\n", b1, u2, r);
+			r = b2.equals(u1);
+			System.out.format("b2==u1 :: %s == %s : %s\n", b2, u1, r);
+			System.out.println("");
+			*/
 		}
 			
 	}
